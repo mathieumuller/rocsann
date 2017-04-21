ca3a4a381a44703dc0ccc6814c941e1d56907472
mathieu muller <mathieu.muller@axiocode.com>
2017-04-21 11:49:08 +0200

    update home templates

--- web/assets/app/js/home.js
+++ web/assets/app/js/home.js
@@ -1,4 +1,13 @@
 $(function(){
-    $('.carousel').carousel();
-    $('.collapsible').collapsible();
+    $('.diaporama').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        adaptiveHeight: true,
+        lazyLoad: 'ondemand',
+        autoplay: true,
+        autoplaySpeed: 5000,
+    });
 });
